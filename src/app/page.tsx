0953586fe72bf3c9b55369033b1e54d2c55d4e82
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-12-13 21:37:39 +0100

    Finished main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 /**
  * v0 by Vercel.
- * @see https://v0.dev/t/5AQtklhLY21
+ * @see https://v0.dev/t/ZJPSBBkMPHT
  */
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
@@ -11,20 +11,30 @@ export default function Component() {
     <div className="flex flex-col min-h-screen bg-[#1D1D1F] dark:bg-gray-900">
       <header className="px-4 lg:px-6 h-16 flex items-center justify-between border-b border-[#0D9488]">
         <Link className="flex items-center justify-center" href="#">
-          <CopyleftIcon className="h-6 w-6" />
+          <img
+            alt="Logo"
+            className="h-6 w-6"
+            height="24"
+            src="/logo.png"
+            style={{
+              aspectRatio: "24/24",
+              objectFit: "cover",
+            }}
+            width="24"
+          />
           <span className="ml-2 text-lg font-bold text-gray-200 dark:text-gray-200">Communauté Events</span>
         </Link>
         <nav className="flex gap-4 sm:gap-6">
-          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="#">
+          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="/">
             Home
           </Link>
-          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="#">
+          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="about">
             About
           </Link>
-          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="#">
+          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="#projects">
             Projects
           </Link>
-          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="#">
+          <Link className="text-sm font-medium text-gray-400 hover:underline dark:text-gray-300" href="contact">
             Contact
           </Link>
         </nav>
@@ -33,6 +43,17 @@ export default function Component() {
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-[#1D1D1F] dark:bg-gray-800">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
+              <img
+                alt="Communauté Events logo"
+                className="h-48 w-auto mb-4"
+                height="192"
+                src="/logo.png"
+                style={{
+                  aspectRatio: "192/192",
+                  objectFit: "cover",
+                }}
+                width="192"
+              />
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl text-gray-200 dark:text-gray-200">
                   Bienvenue chez Communauté Events
@@ -42,67 +63,76 @@ export default function Component() {
                 </p>
               </div>
               <div className="space-x-4">
-                <Button className="bg-[#0D9488] hover:bg-[#36776b] text-white py-2 px-6 rounded-md">
-                  Get Started
+                <Button className="bg-[#0D9488] hover:bg-[#36776b] text-white py-2 px-6 rounded-md">À propos</Button>
+                <a href="https://github.com/Communaute-Events">
+                  <Button className="bg-[#333] hover:bg-[#222] text-white py-2 px-6 rounded-md">
+                  <GithubIcon className="w-4 h-4 mr-2" />
+                  GitHub
                 </Button>
-                <Button
-                  className="border-[#0D9488] text-[#0D9488] hover:bg-[#0D9488] hover:text-white py-2 px-6 rounded-md"
-                  variant="outline"
-                >
-                  Learn More
-                </Button>
+                </a>
               </div>
             </div>
           </div>
         </section>
-        <section className="w-full bg-[#1D1D1F] dark:bg-gray-900 py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full bg-[#1D1D1F] dark:bg-gray-900 py-12 md:py-24 lg:py-32 xl:py-48" id="projects">
           <div className="container px-4 md:px-6">
             <h2 className="text-2xl font-bold mb-8 text-gray-200 dark:text-gray-200">Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-              <Card>
-                <CardHeader>
-                  <BotIcon className="w-4 h-4" />
-                  <h3 className="text-lg font-bold text-black dark:text-black">Bot Discord</h3>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-black dark:text-black">
-                    Events Helper, notre bot Discord, permet d'être notifié lorsqu'un évenement est détecté sur un serveur discord. Il est complétement customizable.
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <ServerIcon className="w-4 h-4" />
-                  <h3 className="text-lg font-bold text-black dark:text-black">Notre serveur Minecraft</h3>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-black dark:text-black">
-                    Nous sommes actuellement entrain de développer un serveur Minecraft pour s'entraîner sur plusieurs épreuves récurrentes dans les évenements.
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <AppWindowIcon className="w-4 h-4" />
-                  <h3 className="text-lg font-bold text-black dark:text-black">AutoLaunch</h3>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-black dark:text-black">
-                    Une application de bureau permettant de rapidement lancer Minecraft dans la bonne version, avec les bons mods lorsqu'un évent est détecté.
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <WebhookIcon className="w-4 h-4" />
-                  <h3 className="text-lg font-bold text-black dark:text-black">Events Monitor</h3>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-black dark:text-black">
-                    Un websocket, fournissant des données sur de nombreux serveurs d'évents se trouvants sur Discord.
-                  </p>
-                </CardContent>
-              </Card>
+              <a className="group" href="https://github.com/Communaute-Events/ce-discord-bot">
+                <Card className="transform transition-all duration-200 ease-in-out hover:scale-110">
+                  <CardHeader>
+                    <BotIcon className="w-4 h-4" />
+                    <h3 className="text-lg font-bold text-black dark:text-black">Bot Discord</h3>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-black dark:text-black">
+                      Events Helper, notre bot Discord, permet d'être notifié lorsqu'un évenement est détecté sur un
+                      serveur discord. Il est complétement customizable.
+                    </p>
+                  </CardContent>
+                </Card>
+              </a>
+              <a className="group" href="#projects">
+                <Card className="transform transition-all duration-200 ease-in-out hover:scale-110">
+                  <CardHeader>
+                    <ServerIcon className="w-4 h-4" />
+                    <h3 className="text-lg font-bold text-black dark:text-black">Notre serveur Minecraft</h3>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-black dark:text-black">
+                      Coming soon: Nous sommes actuellement entrain de développer un serveur Minecraft pour s'entraîner sur plusieurs
+                      épreuves récurrentes dans les évenements.
+                    </p>
+                  </CardContent>
+                </Card>
+              </a>
+              <a className="group" href="https://github.com/Communaute-Events/AutoLaunch">
+                <Card className="transform transition-all duration-200 ease-in-out hover:scale-110">
+                  <CardHeader>
+                    <AppWindowIcon className="w-4 h-4" />
+                    <h3 className="text-lg font-bold text-black dark:text-black">AutoLaunch</h3>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-black dark:text-black">
+                      Une application de bureau permettant de rapidement lancer Minecraft dans la bonne version, avec
+                      les bons mods lorsqu'un évent est détecté.
+                    </p>
+                  </CardContent>
+                </Card>
+              </a>
+              <a className="group" href="https://github.com/Communaute-Events/ce-event-monitor">
+                <Card className="transform transition-all duration-200 ease-in-out hover:scale-110">
+                  <CardHeader>
+                    <WebhookIcon className="w-4 h-4" />
+                    <h3 className="text-lg font-bold text-black dark:text-black">Events Monitor</h3>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-black dark:text-black">
+                      Un websocket, fournissant des données sur de nombreux serveurs d'évents se trouvants sur Discord.
+                    </p>
+                  </CardContent>
+                </Card>
+              </a>
             </div>
           </div>
         </section>
@@ -170,7 +200,7 @@ function BotIcon(props) {
 }
 
 
-function CopyleftIcon(props) {
+function GithubIcon(props) {
   return (
     <svg
       {...props}
@@ -184,8 +214,8 @@ function CopyleftIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M9 9.35a4 4 0 1 1 0 5.3" />
+      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+      <path d="M9 18c-4.51 2-5-2-7-2" />
     </svg>
   )
 }
